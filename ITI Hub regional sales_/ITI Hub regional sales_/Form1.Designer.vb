8c5fe65d8dce044ad6fa1110d9a4b9c906d8ec52
Khaya Ngwenya <65853544+Khayaa@users.noreply.github.com>
2022-08-22 00:21:43 +0200

    Add files via upload

---
+++ ITI Hub regional sales_/ITI Hub regional sales_/Form1.Designer.vb
@@ -0,0 +1,275 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Displaybtn = New System.Windows.Forms.Button()
+        Me.closebtn = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.tb1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.tb2 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.tb3 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.tb4 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.tb5 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.tb6 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.tb7 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Displaybtn
+        '
+        Me.Displaybtn.Location = New System.Drawing.Point(160, 374)
+        Me.Displaybtn.Name = "Displaybtn"
+        Me.Displaybtn.Size = New System.Drawing.Size(106, 50)
+        Me.Displaybtn.TabIndex = 0
+        Me.Displaybtn.Text = "Display"
+        Me.Displaybtn.UseVisualStyleBackColor = True
+        '
+        'closebtn
+        '
+        Me.closebtn.Location = New System.Drawing.Point(297, 374)
+        Me.closebtn.Name = "closebtn"
+        Me.closebtn.Size = New System.Drawing.Size(106, 50)
+        Me.closebtn.TabIndex = 1
+        Me.closebtn.Text = "Close"
+        Me.closebtn.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Font = New System.Drawing.Font("Microsoft YaHei", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(2, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 26)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Total Sales"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label2.Font = New System.Drawing.Font("Microsoft YaHei", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(0, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(176, 26)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Contribution in (%)"
+        '
+        'tb1
+        '
+        Me.tb1.Location = New System.Drawing.Point(314, 82)
+        Me.tb1.Name = "tb1"
+        Me.tb1.Size = New System.Drawing.Size(100, 22)
+        Me.tb1.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(182, 87)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 17)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Gauteng"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(182, 115)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(97, 17)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Kwazulu Natal"
+        '
+        'tb2
+        '
+        Me.tb2.Location = New System.Drawing.Point(314, 110)
+        Me.tb2.Name = "tb2"
+        Me.tb2.Size = New System.Drawing.Size(100, 22)
+        Me.tb2.TabIndex = 6
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(182, 143)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(98, 17)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Western Cape"
+        '
+        'tb3
+        '
+        Me.tb3.Location = New System.Drawing.Point(314, 138)
+        Me.tb3.Name = "tb3"
+        Me.tb3.Size = New System.Drawing.Size(100, 22)
+        Me.tb3.TabIndex = 8
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.tb5)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.tb6)
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.tb7)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(158, 203)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(402, 166)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Location = New System.Drawing.Point(158, 31)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(402, 166)
+        Me.GroupBox2.TabIndex = 11
+        Me.GroupBox2.TabStop = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(24, 134)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(46, 18)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Total"
+        '
+        'tb4
+        '
+        Me.tb4.Location = New System.Drawing.Point(314, 166)
+        Me.tb4.Name = "tb4"
+        Me.tb4.Size = New System.Drawing.Size(100, 22)
+        Me.tb4.TabIndex = 12
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(24, 107)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(98, 17)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "Western Cape"
+        '
+        'tb5
+        '
+        Me.tb5.Location = New System.Drawing.Point(156, 102)
+        Me.tb5.Name = "tb5"
+        Me.tb5.Size = New System.Drawing.Size(100, 22)
+        Me.tb5.TabIndex = 14
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(24, 79)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(97, 17)
+        Me.Label8.TabIndex = 13
+        Me.Label8.Text = "Kwazulu Natal"
+        '
+        'tb6
+        '
+        Me.tb6.Location = New System.Drawing.Point(156, 74)
+        Me.tb6.Name = "tb6"
+        Me.tb6.Size = New System.Drawing.Size(100, 22)
+        Me.tb6.TabIndex = 12
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(24, 51)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(63, 17)
+        Me.Label9.TabIndex = 11
+        Me.Label9.Text = "Gauteng"
+        '
+        'tb7
+        '
+        Me.tb7.Location = New System.Drawing.Point(156, 46)
+        Me.tb7.Name = "tb7"
+        Me.tb7.Size = New System.Drawing.Size(100, 22)
+        Me.tb7.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(597, 451)
+        Me.Controls.Add(Me.tb4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.tb3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.tb2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.tb1)
+        Me.Controls.Add(Me.closebtn)
+        Me.Controls.Add(Me.Displaybtn)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ITI Hub regionals sales"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Displaybtn As Button
+    Friend WithEvents closebtn As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents tb1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents tb2 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents tb3 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents tb5 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents tb6 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents tb7 As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents tb4 As TextBox
+End Class
---
+++ ITI Hub regional sales_/ITI Hub regional sales_/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+
+    Private Sub Displaybtn_Click(sender As Object, e As EventArgs) Handles Displaybtn.Click
+        Dim ts(3, 6), totalsales, tsgp(6), tskzn(6), tswc(6), gp, kzn, wc, pgp, pkzn, pwc As Integer
+        '''Use Array intializer to initialize ts(3,6) for Multi-dimensional Array
+        tsgp(0) = 90000
+        tsgp(1) = 85000
+        tsgp(2) = 80000
+        tsgp(3) = 83000
+        tsgp(4) = 87000
+        tsgp(5) = 80000
+
+        tskzn(0) = 120000
+        tskzn(1) = 190000
+        tskzn(2) = 175000
+        tskzn(3) = 188000
+        tskzn(4) = 125000
+        tskzn(5) = 163000
+
+        tswc(0) = 65000
+        tswc(1) = 64000
+        tswc(2) = 71000
+        tswc(3) = 67000
+        tswc(4) = 65000
+        tswc(5) = 64000
+
+        For i As Integer = 0 To tsgp.Length - 1 ''// tsqp.lenght
+            gp = gp + tsgp(i)
+            wc = wc + tswc(i)
+            kzn = kzn + tskzn(i)
+        Next i
+        totalsales = gp + wc + kzn
+        tb1.Text = "R " & gp
+        tb2.Text = "R " & kzn
+        tb3.Text = "R " & wc
+        tb4.Text = "R " & totalsales
+
+        pgp = gp / totalsales * 100
+        pwc = wc / totalsales * 100
+        pkzn = kzn / totalsales * 100
+
+        tb7.Text = pgp & " %"
+        tb6.Text = pkzn & " %"
+        tb5.Text = pwc & " %"
+
+    End Sub
+
+    Private Sub closebtn_Click(sender As Object, e As EventArgs) Handles closebtn.Click
+        Me.Close()
+    End Sub
+End Class
